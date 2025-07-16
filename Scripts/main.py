22eec7e43e60a828960a2525800751e71688fa79
hiyuki0302 <crypto.streeeet@gmail.com>
2025-07-16 14:18:04 +0900

    RSI計算、データフレーム化

--- Scripts/main.py
+++ Scripts/main.py
@@ -3,6 +3,7 @@ import json
 import asyncio
 import sys
 import pandas as pd
+from analysis import rsi_analysis
 
 class kline:
     def __init__(self, symbol:str, client:pybotters.Client):
@@ -14,7 +15,7 @@ class kline:
         self.base_url = "https://api.bybit.com"
 
     async def get_kline(self):
-        """ローソク足取得"""
+        """ローソク足取得、データフレームに変換"""
         endpoint = "/v5/market/kline"
         url = f"{self.base_url}{endpoint}"
         params = {
@@ -26,7 +27,12 @@ class kline:
         result = await self.client.fetch("GET", url=url, params=params)
         text = result.text
         data = json.loads(text)
-        return data
+        data = data.get('result', {}).get('list', [])
+        df = pd.DataFrame(data, columns=["timestamp", "open", "high", "low", "close", "volume", "quote_volume"]).astype(float)
+        df["timestamp"] = pd.to_datetime(df["timestamp"].astype("int64"), unit='ms', utc=True) + pd.Timedelta(hours=9)
+        df['RSI'] = rsi_analysis(data=df)
+        print(df.dropna())
+        return df
 
 async def run_task(symbol, client:pybotters.Client):
     bot = kline(symbol, client)
