dc54313f06f57cced1cb9c01a62ddd930ce997ec
hiyuki0302 <crypto.streeeet@gmail.com>
2025-07-16 17:02:31 +0900

    ATR計算

--- Scripts/analysis.py
+++ Scripts/analysis.py
@@ -11,3 +11,13 @@ def rsi_analysis(data, period:int=14):
     rs = avg_gain / avg_loss
     rsi = 100 - (100 / (1 + rs))
     return rsi
+
+def atr_analysis(data):
+    """下の計算なんか違いますわ！"""
+    array_high = data['high'].values
+    array_low = data['low'].values
+    array_close = data['close'].values
+    
+    tr1 = (array_high[1:] - array_low[1:])
+    tr2 = abs(array_high[1:] - array_close[0:-1])
+    tr3 = abs(array_low[1:] - array_close[0:-1])
