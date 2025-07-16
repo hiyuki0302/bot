a483476eb5f4c23abb46806329795ebbfd806e26
hiyuki0302 <crypto.streeeet@gmail.com>
2025-07-16 14:04:07 +0900

    RSI計算追加

---
+++ Scripts/analysis.py
@@ -0,0 +1,13 @@
+
+def rsi_analysis(data, period:int=14):
+    """RSI計算"""
+    delta = data['close'].diff()
+    gain = delta.where(delta > 0, 0)
+    loss = -delta.where(delta < 0, 0)
+    
+    avg_gain = gain.rolling(window=period).mean()
+    avg_loss = loss.rolling(window=period).mean()
+    
+    rs = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
